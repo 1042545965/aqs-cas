1b067c363d97f12aec67203042a488d16a35dd64
conlon <conlon.duan@starlight-sms.com>
2022-06-27 09:43:56 +0800

    FutureTask

--- src/main/java/com/study/future/FutureTaskPollDemo.java
+++ src/main/java/com/study/future/FutureTaskPollDemo.java
@@ -6,34 +6,59 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class FutureTaskPollDemo {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         // 线程池
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         Long startTime = System.currentTimeMillis();
 
         // m1();
         FutureTask<Object> futureTask1 = new FutureTask<>(
-            ()-> {TimeUnit.SECONDS.sleep(5L);System.out.println("hello futureTask1 over");return "futureTask1 over";}
+            () -> {
+                TimeUnit.SECONDS.sleep(5L);
+                System.out.println("hello futureTask1 ");
+                return "futureTask1 over";
+            }
         );
         executorService.execute(futureTask1);
-        System.out.println(futureTask1.get());
+
         FutureTask<Object> futureTask2 = new FutureTask<>(
-            ()-> {TimeUnit.SECONDS.sleep(4L);
-                System.out.println("hello futureTask2 over");return "futureTask2 over";}
+            () -> {
+                TimeUnit.SECONDS.sleep(4L);
+                System.out.println("hello futureTask2 ");
+                return "futureTask2 over";
+            }
         );
         executorService.execute(futureTask2);
-        System.out.println(futureTask2.get());
+
         FutureTask<Object> futureTask3 = new FutureTask<>(
-            ()-> {TimeUnit.SECONDS.sleep(3L);System.out.println("hello futureTask3 over");return "futureTask3 over";}
+            () -> {
+                TimeUnit.SECONDS.sleep(3L);
+                System.out.println("hello futureTask3 ");
+                return "futureTask3 over";
+            }
         );
         executorService.execute(futureTask3);
+        System.out.println(futureTask1.get());
+        System.out.println(futureTask2.get());
         System.out.println(futureTask3.get());
-        Long endTime = System.currentTimeMillis();
-        executorService.shutdown();
-        System.out.println("耗时 ： " + (endTime - startTime));
+        // 抛出异常 ， 避免阻塞 不够优雅
+//        System.out.println(futureTask3.get(2 , TimeUnit.SECONDS));
+//        // 通过死循环 ， 空转CPU 消耗性能 但是不会被阻塞了
+//        while (true){
+//            if (futureTask1.isDone()){
+//                System.out.println(futureTask1.get());
+//                break;
+//            }else {
+//                System.out.println("在处理......");
+//            }
+//        }
+//        Long endTime = System.currentTimeMillis();
+//        executorService.shutdown();
+//        System.out.println("main 耗时 ： " + (endTime - startTime));
     }
 
 
